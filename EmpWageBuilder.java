6e02068dc2281dc68d5d903eff5f70923caffd7f
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-23 23:16:40 +0530

    [Umesh3103]Added: calculating wage for 20 days in a month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -115,3 +115,38 @@ public class EmpWageBuilderUC5{
 		System.out.println("Total Emp Wage: " +totalEmpWage);
 	}
 }
+
+// calculating wage till total working days and working hours per month is reached
+
+public class EmpWageBuilderUC6{
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;	
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
+	public static void main(String[] args){
+		
+		int empHrs=0;
+		int totalEmpHrs=0,totalWorkingDays=0;
+		
+		while(totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+			totalWorkingDays++;
+			int empCheck=(int) Math.floor(Math.random()*10) %3;
+			switch(empCheck){
+				case IS_FULL_TIME:
+					empHrs=8;
+					break;
+				case IS_PART_TIME:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+				
+				}
+			totalEmpHrs += empHrs;
+			System.out.println("Day#: " +totalWorkingDays + " Emp Hr: " +empHrs);
+		}
+		int totalEmpWage= totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage: " +totalEmpWage);
+	}
+}
