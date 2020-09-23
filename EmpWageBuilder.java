c24b2ef6526571962af9b31942f18e26f9a9e924
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-09-23 22:11:30 +0530

    [Umesh3103] git status git commit -m [Umesh3103]

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,3 +4,18 @@ public class EmpWageBuilderWelcome{
 		System.out.println("Welcome to employee wage problem");
 	}
 }
+
+// Employee present or  absent
+public class EmpWageBuilderUC1{
+	public static void main(String[] args){
+		int IS_FULL_TIME =1;
+		double empCheck =Math.floor(Math.random() *10) %2;
+		if(empCheck == IS_FULL_TIME){
+				System.out.println("Employee is Present");
+		}
+		else
+			System.out.println("Employee is Absent");
+	}
+}
+
+
